86101f378445c7fa79c9c2606d8da3d1cca64dc1
raizy21 <chiper_raizy@yahoo.com>
2021-01-06 16:42:39 +0100

    init-custom_countdown

---
+++ script.js
@@ -0,0 +1,14 @@
+/***************
+ *  Links
+ * {@link} - https://pixabay.com/videos/
+ * {@link} - https://www.youcompress.com/
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/forms/Basic_form_hints
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/date
+ * {@link} - https://www.w3schools.com/jsref/jsref_obj_date.asp
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
+ * {@link} - https://www.w3schools.com/jsref/jsref_split.asp
+ * {@link} - https://www.w3schools.com/js/js_timing.asp
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
+ *
+ ********************/
